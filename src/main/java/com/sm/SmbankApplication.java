ebd7a26f74253481241b73aedf407e71a70aac5f
01ight <=>
2016-11-06 22:37:31 +0100

    Créeation d'entités

--- src/main/java/com/sm/SmbankApplication.java
+++ src/main/java/com/sm/SmbankApplication.java
@@ -7,6 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SmbankApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SmbankApplication.class, args);
+		SpringApplication.run(SmbankApplication.class, args);J
 	}
 }
